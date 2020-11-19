34146084c7bf4808912a9a0c5eaa30069560527f
xstrba <borisstrbak@gmail.com>
2020-11-19 15:20:35 +0100

    feat: users seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -39,6 +39,7 @@ final class UserSeeder extends \Illuminate\Database\Seeder
     public function run(): void
     {
         $user = $this->usersRepository->findByNickname('root');
+        $faker = \Faker\Factory::create('cs');
 
         if (!$user) {
             $this->usersRepository->create([
@@ -48,6 +49,36 @@ final class UserSeeder extends \Illuminate\Database\Seeder
                 User::ATTR_ROLE => \App\Enums\RolesEnum::ROLE_ADMINISTRATOR,
                 User::ATTR_PASSWORD => $this->hasher->make('123456'),
                 User::ATTR_BIRTH => \Carbon\Carbon::now()->subYears(40),
+            ]);
+        }
+
+        foreach (range(1,10) as $i) {
+            $role = \App\Enums\RolesEnum::ROLE_STUDENT;
+            if ($i < 3) {
+                $role = \App\Enums\RolesEnum::ROLE_PROFESSOR;
+                $date = $faker->dateTimeBetween('-70 years', '-30 years');
+            } else if ($i < 6) {
+                $role = \App\Enums\RolesEnum::ROLE_ASSISTANT;
+                $date = $faker->dateTimeBetween('-50 years', '-25 years');
+            } else {
+                $date = $faker->dateTimeBetween('-50 years', '-18 years');
+            }
+
+            $this->usersRepository->create([
+                User::ATTR_FIRST_NAME => $faker->firstName,
+                User::ATTR_LAST_NAME => $faker->lastName,
+                User::ATTR_EMAIL => $faker->email,
+                User::ATTR_NICKNAME => $faker->unique()->userName,
+                User::ATTR_ROLE => $role,
+                User::ATTR_PASSWORD => $this->hasher->make('123456'),
+                User::ATTR_BIRTH => $date,
+                User::ATTR_STREET => $faker->streetName,
+                User::ATTR_HOUSE_NUMBER => $faker->buildingNumber,
+                User::ATTR_CITY => $faker->city,
+                User::ATTR_COUNTRY => $faker->randomElement(\App\Enums\CountriesEnum::instance()->getValues()),
+                User::ATTR_GENDER => $faker->randomElement(\App\Enums\GendersEnum::instance()->getValues()),
+                User::ATTR_LANGUAGE => \App\Enums\LanguagesEnum::CZ,
+                User::ATTR_PHONE => $faker->phoneNumber,
             ]);
         }
     }
